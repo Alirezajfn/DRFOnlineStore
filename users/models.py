c46de9680bdaaf5b74e85f6a980002b60e03cc08
alirezajfn <alirezajfn1@gmail.com>
2023-10-11 15:44:48 +0330

    create custom user model and managers

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from users.managers import CustomUserManager
+
+
+class User(AbstractUser):
+    objects = CustomUserManager()
+    REQUIRED_FIELDS = []
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['email', ]),
+        ]
+
