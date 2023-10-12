e46e5c33c3d2adcd3ea70571461f366a11195d6d
alirezajfn <alirezajfn1@gmail.com>
2023-10-13 01:02:59 +0330

    add category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', blank=True, null=True)
+    slug = models.SlugField(unique=True, max_length=255)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.parent}/{self.name}"
