2ab3a3aeef1589e5e5ea3fc346239dc685a2d4f5
alirezajfn <alirezajfn1@gmail.com>
2023-10-12 13:13:23 +0330

    add regiser view and serializers

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,5 +1,12 @@
+from rest_framework.routers import DefaultRouter
+
+from users.api.views import UserViewSet
+
 app_name = 'users'
+
+router = DefaultRouter()
+router.register('', UserViewSet, basename='user')
 
 urlpatterns = [
 
-]
+] + router.urls
