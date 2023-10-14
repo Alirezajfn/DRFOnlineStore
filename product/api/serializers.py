dd0337aa92d2b28277f48e0af5475614036ecd09
alirezajfn <alirezajfn1@gmail.com>
2023-10-14 12:05:34 +0330

    implement product url, views and serializers

---
+++ product/api/serializers.py
@@ -0,0 +1,170 @@
+from rest_framework import serializers
+
+from category.models import Category
+from product.models import Product, ProductImage
+
+
+class ProductCreateSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(read_only=True)
+    create_time = serializers.DateTimeField(read_only=True)
+    modified_time = serializers.DateTimeField(read_only=True)
+    creator = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    categories = serializers.ListField(
+        child=serializers.CharField(),
+        required=True,
+        write_only=True
+    )
+    categories_list = serializers.SerializerMethodField(read_only=True)
+    main_image = serializers.ImageField(required=True)
+    images = serializers.ListField(
+        child=serializers.ImageField(),
+        required=False,
+        write_only=True
+    )
+    images_list = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'description',
+            'price',
+            'stock_quantity',
+            'categories',
+            'creator',
+            'slug',
+            'create_time',
+            'modified_time',
+            'main_image',
+            'images',
+            'categories_list',
+            'images_list',
+        ]
+
+    def get_categories_list(self, obj):
+        return obj.categories.values_list('slug', flat=True)
+
+    def get_images_list(self, obj):
+        return obj.images.values_list('image', flat=True)
+
+    def validate(self, attrs):
+        categories = attrs.get('categories')
+        for category in categories:
+            if not Category.objects.filter(slug=category).exists():
+                raise serializers.ValidationError('Category does not exist')
+
+        return attrs
+
+    def create(self, validated_data):
+        category_data = validated_data.pop('categories')
+        images = validated_data.pop('images', None)
+        product = Product.objects.create(**validated_data)
+
+        for category in category_data:
+            category = Category.objects.filter(slug=category).first()
+            if category:
+                product.categories.add(category)
+
+        if images:
+            for image in images:
+                ProductImage.objects.create(product=product, image=image)
+        return product
+
+
+class ProductUpdateSerializer(serializers.ModelSerializer):
+    categories = serializers.ListField(
+        child=serializers.CharField(),
+        required=True,
+        write_only=True
+    )
+    categories_list = serializers.SerializerMethodField(read_only=True)
+    images = serializers.ListField(
+        child=serializers.ImageField(),
+        required=False,
+        write_only=True
+    )
+    images_list = serializers.SerializerMethodField(read_only=True)
+    main_image = serializers.ImageField(required=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'description',
+            'price',
+            'stock_quantity',
+            'categories',
+            'categories_list',
+            'main_image',
+            'images',
+            'images_list',
+        ]
+
+    def validate(self, attrs):
+        categories = attrs.get('categories')
+        if categories:
+            for category in categories:
+                if not Category.objects.filter(slug=category).exists():
+                    raise serializers.ValidationError('Category does not exist')
+
+        return attrs
+
+    def get_categories_list(self, obj):
+        return obj.categories.values_list('slug', flat=True)
+
+    def get_images_list(self, obj):
+        return obj.images.values_list('image', flat=True)
+
+    def update(self, instance, validated_data):
+        category_data = validated_data.pop('categories', None)
+        images = validated_data.pop('images', None)
+        super().update(instance, validated_data)
+
+        if category_data:
+            instance.categories.clear()
+            for category in category_data:
+                category = Category.objects.filter(slug=category).first()
+                if category:
+                    instance.categories.add(category)
+
+        if images:
+            instance.images.all().delete()
+            for image in images:
+                ProductImage.objects.create(product=instance, image=image)
+        return instance
+
+
+class ProductReadOnlySerializer(serializers.ModelSerializer):
+    categories = serializers.SerializerMethodField(read_only=True)
+    images = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'slug',
+            'description',
+            'price',
+            'stock_quantity',
+            'categories',
+            'create_time',
+            'modified_time',
+            'main_image',
+            'images',
+            'sales_count',
+        ]
+        read_only_fields = fields
+
+    # remove images when action is list
+    def get_fields(self):
+        if self.context['view'].action == 'list':
+            fields = super(ProductReadOnlySerializer, self).get_fields()
+            fields.pop('images')
+            return fields
+        return super(ProductReadOnlySerializer, self).get_fields()
+
+    def get_categories(self, obj):
+        return obj.categories.values_list('slug', flat=True)
+
+    def get_images(self, obj):
+        return obj.images.values_list('image', flat=True)
--- product/api/views.py
+++ product/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from product.api.serializers import ProductCreateSerializer, ProductReadOnlySerializer, ProductUpdateSerializer
+from product.models import Product
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    lookup_field = 'slug'
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return ProductCreateSerializer
+        elif self.action in ['update', 'partial_update']:
+            return ProductUpdateSerializer
+        return ProductReadOnlySerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = (AllowAny,)
+        else:
+            # TODO: Add IsOwner permission
+            permission_classes = (IsAdminUser,)
+        return [permission() for permission in permission_classes]
+
+
+class ProductCategoryListView(ListAPIView):
+    serializer_class = ProductReadOnlySerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        return Product.objects.filter(category__slug=category_slug)
