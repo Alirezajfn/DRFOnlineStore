6c64cf04b809a0383577eead287a76916fbe0a25
alirezajfn <alirezajfn1@gmail.com>
2023-10-15 23:39:41 +0330

    change on_delete parent category to set null

--- category/models.py
+++ category/models.py
@@ -6,12 +6,11 @@ from django.utils.text import slugify
 class Category(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField(max_length=3000, null=True, blank=True)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', blank=True, null=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='children', blank=True, null=True)
     slug = models.SlugField(unique=True, max_length=255)
     creator = models.ForeignKey(to=get_user_model(), on_delete=models.PROTECT, related_name='categories')
     create_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
-
 
     def save(self, *args, **kwargs):
         if not self.slug:
