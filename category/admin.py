84fa5b6ebc45c9b6d706636f18c3bd8f7a829dc7
alirezajfn <alirezajfn1@gmail.com>
2023-10-16 13:33:59 +0330

    create categories admin for category management

---
+++ category/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from category.models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent', 'slug', 'create_time']
+    list_filter = ['parent']
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ['name', 'parent']
+    fields = ['name', 'parent', 'description', 'slug', 'create_time', 'modified_time', 'creator']
+    readonly_fields = ['create_time', 'modified_time']
