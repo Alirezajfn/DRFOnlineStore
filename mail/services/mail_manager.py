faf4aef42bcfeb4ce004617a1cf5e5fc9b8f64ad
alirezajfn <alirezajfn1@gmail.com>
2023-10-18 13:27:33 +0330

    documenting mail sender manager

--- mail/services/mail_manager.py
+++ mail/services/mail_manager.py
@@ -8,13 +8,25 @@ from django.core.mail.backends.smtp import EmailBackend
 
 logger = logging.getLogger(__name__)
 
+
 class MailSenderManager:
+    """
+    This class is responsible for sending emails using django core mail
+    """
     def __init__(self, connection: EmailBackend = None):
         self.connection = connection
         if self.connection is None:
             self.connection = get_connection(settings.EMAIL_BACKEND)
 
     def send(self, to_email: list, message: str, title: str, **kwargs):
+        """
+        This method is responsible for sending emails using django core mail
+        :param to_email: list of emails
+        :param message: message of email
+        :param title: title of email
+        :param kwargs: other arguments
+        :return: number of successful emails
+        """
         try:
             successes = django_mail.send_mail(subject=title,
                                               message=message,
