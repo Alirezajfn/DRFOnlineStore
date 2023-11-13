b76f5b603ecd41de3727e9f07bd8f171d9bbbf2a
Alireza Jafarinejad <alirezajfn1@gmail.com>
2023-11-13 11:55:09 +0330

    implement get_all_urls function for dynamic URL retrieval

---
+++ permissions/services/get_urls.py
@@ -0,0 +1,26 @@
+from django.urls import get_resolver
+from django.urls.resolvers import URLPattern, URLResolver
+
+
+def get_all_urls():
+
+    resolver = get_resolver()
+    urls = []
+
+    def _get_urls(patterns, prefix=''):
+        for pattern in patterns:
+            if isinstance(pattern, URLResolver):
+                _get_urls(pattern.url_patterns, prefix + pattern.pattern.regex.pattern)
+            elif isinstance(pattern, URLPattern):
+                if hasattr(pattern.callback, 'codename'):
+                    codename = pattern.callback.codename
+                else:
+                    codename = pattern.name
+
+                urls.append({'url': prefix + pattern.pattern.regex.pattern,
+                             'codename': codename,
+                             'view_name': pattern.callback.__name__})
+
+    _get_urls(resolver.url_patterns)
+
+    return urls
