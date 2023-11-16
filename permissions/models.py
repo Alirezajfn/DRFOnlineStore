b093c731e9bfe22c17e2d0e34ed9d16264f21341
Alireza Jafarinejad <alirezajfn1@gmail.com>
2023-11-16 12:24:56 +0330

    add group of urls in permissions

--- permissions/models.py
+++ permissions/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 
+class UrlsGroup(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class PermissionPerUrls(models.Model):
     url = models.CharField(max_length=255)
     codename = models.CharField(max_length=255, unique=True, db_index=True)
     view = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     description = models.TextField(blank=True, null=True)
+    group = models.ForeignKey(UrlsGroup, on_delete=models.CASCADE, related_name='permissions', null=True, blank=True)
 
     def __str__(self):
         return self.codename
