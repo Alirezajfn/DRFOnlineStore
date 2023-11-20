ac232c8e0937cfbf9b7ab16fb7a7387a5b2a61eb
Alireza Jafarinejad <alirezajfn1@gmail.com>
2023-11-20 13:15:05 +0330

    add role to user model

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -33,6 +33,7 @@ class Migration(migrations.Migration):
                 ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
                 ('permissions_per_url', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='permissions.permissionperurls', verbose_name='user permissions per url')),
                 ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                ('role', models.ManyToManyField(blank=True, help_text='Specific roles for this user.', related_name='user_set', related_query_name='user', to='permissions.role', verbose_name='roles')),
             ],
             options={
                 'indexes': [models.Index(fields=['email'], name='users_user_email_6f2530_idx')],
--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from permissions.models import PermissionPerUrls
+from permissions.models import PermissionPerUrls, Role
 from users.managers import CustomUserManager
 
 
@@ -15,6 +15,15 @@ class CustomPermissionMixin(models.Model):
         verbose_name=_("user permissions per url"),
         blank=True,
         help_text=_("Specific permissions for this user."),
+        related_name="user_set",
+        related_query_name="user",
+    )
+
+    role = models.ManyToManyField(
+        Role,
+        verbose_name=_("roles"),
+        blank=True,
+        help_text=_("Specific roles for this user."),
         related_name="user_set",
         related_query_name="user",
     )
